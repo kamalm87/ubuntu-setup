261a8206ba0890b8c40c777dc76960d75e2dd17f
kamalm87 <kamalm.kamal@gmail>
2016-02-15 12:50:49 -0500

    added some more desired items

--- npm.sh
+++ npm.sh
@@ -1,4 +1,4 @@
 sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
-sudo npm install -g grunt-cli yo bower gulp typescript
+sudo npm install -g grunt-cli yo bower gulp typescript compass
--- python.sh
+++ python.sh
@@ -1,3 +1,3 @@
-sudo apt-get install python3 python3-pip postgresql libpq-dev postgresql-9.4 pgadmin3 python-pip pgadmin3
-sudo pip3 install numpy scipy pandas beautifulsoup4 psycopg2 sqlalchemy flask 
-sudo pip2 install numpy scipy pandas beautifulsoup4 psycopg2 sqlalchemy flask 
+sudo apt-get install python3 python3-pip postgresql libpq-dev postgresql-9.4 pgadmin3 python-pip pgadmin3 ruby-full
+sudo pip3 install numpy scipy pandas beautifulsoup4 psycopg2 sqlalchemy flask virtualenv 
+sudo pip2 install numpy scipy pandas beautifulsoup4 psycopg2 sqlalchemy flask virtualenv
